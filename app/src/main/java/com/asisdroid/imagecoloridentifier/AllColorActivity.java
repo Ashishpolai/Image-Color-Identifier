f1e210ee5e0a4cef1e2a0c89160e74ca6075461d
AshishKumar Polai <ashishkumar.polai@rockyou.com>
2019-05-25 16:01:51 +0530

    no message

--- app/src/main/java/com/asisdroid/imagecoloridentifier/AllColorActivity.java
+++ app/src/main/java/com/asisdroid/imagecoloridentifier/AllColorActivity.java
@@ -72,6 +72,8 @@ public class AllColorActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_all_color);
 
         mInterstitialAd = new InterstitialAd(this);
